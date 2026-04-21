c32b925c09656f6e0f5108a7edb9c9642e92c54e
vivek yadav <vivekyadav@viveks-MacBook-Pro.local>
2026-04-20 20:47:40 -0700

    fix: use HashRouter for GitHub Pages compatibility

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import LibraryPage from './pages/LibraryPage';
